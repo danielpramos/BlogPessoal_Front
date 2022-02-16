f41c234511e20d8a5caee181d8c82e0dde12b8ae
Daniel Pinheiro Ramos <daniel.ramos5291@gmail.com>
2022-02-15 23:23:25 -0300

    Criação Navbar | Home | Footer com Reactjs e Material ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,18 @@
 import React from 'react';
+import Navbar from './components/static/navbar/Navbar';
+import Footer from './components/static/footer/Footer';
 import './App.css';
 import Home from './paginas/home/Home';
 
 
+
 function App() {
   return (
-    <Home />
+    <>
+      <Navbar />
+      <Home />
+      <Footer />
+    </>
   );
 };
 
